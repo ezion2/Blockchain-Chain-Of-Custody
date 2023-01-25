12e1463de550f584fa8dfe3f0b0bb3b8d0959eb9
Trevor Ransom <transom1@outlook.com>
2023-01-25 12:07:42 -0700

    first commit with CLI and parser files

---
+++ cli.py
@@ -0,0 +1,27 @@
+import argparse
+
+from parser import block_chain
+
+
+class Parser:
+
+    @staticmethod
+    def build_args():
+        """
+        Constructs command line arguments for the chain-of-custody tool
+        """
+        parser = argparse.ArgumentParser(
+            description="Parser for chain-of-custody tool"
+        )
+        parser.add_argument(
+            "--no-banner",
+            action="store_true",
+            default=False,
+            dest="no_banner",
+            help="Do not display banner",
+        )
+        subparsers = parser.add_subparsers()
+        chain = subparsers.add_parser('bchoc', help='Block-Chain-of-Custody')
+        chain.set_defaults(func=block_chain)
+
+        return parser.parse_args()
---
+++ parser.py
@@ -0,0 +1,8 @@
+import subprocess as sp
+
+def block_chain():
+    """
+    Checks what arguments are passed in and adds evidence to evidence table
+    Returns:
+        Nothing
+    """
